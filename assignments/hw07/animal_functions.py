98326205649a724ebbeabc2f2186dbd02a3473ff
Joe Versoza <jversoza@cs.nyu.edu>
2020-03-31 10:27:01 -0400

    add sort by name

--- assignments/hw07/animal_functions.py
+++ assignments/hw07/animal_functions.py
@@ -1,11 +1,12 @@
 """
 animal_functions.py - 12 points
 =====
-Write three functions that work with nested lists:
+Write four functions that work with nested lists:
 
 1. find_by_name(name, animals)
 2. stringify_animals(animals)
 3. get_most_urgent(animals)
+4. sort_by_name
 
 The parameter, animals, will be a list of lists representing a collection 
 of animals at veterinary clinic (animal hospital!). The outer list is all 
@@ -103,4 +104,60 @@ animal_list = [['sam', 'snake', 4], ['gertrude', 'goat', 99]]
 print(get_most_urgent(animal_list))
 # will print out ['gertrude', 'goat', 99]
 
+
+sort_by_name:
+-----
+This function will return a new list of animals, sorted by the name of the 
+the animal (that is, the first element in the list that represents a single 
+animal), based on the original list of animals given to the function. The
+result will be a new list of animals sorted alphabetically by name.
+
+* parameters: a list of animals (a list of lists)
+* processing:
+    1. DO NOT USE THE LIST'S SORT METHOD, instead, follow this process
+    2. go through every animal in the list positionally (that is, look at the
+       first animal, the second, third, etc...)
+    3. compare the name of the current animal with the name of the next animal
+    4. if the name of the current animal goes after the name of th next animal,
+       swap animals
+    5. remember, the idiomatic way of swapping in python is a, b = b, a (but
+       be careful, as you'll have to swap two elements in a list!)
+    6. of course, once you're at the last element, there's no next element to 
+       check
+    7. once you've gone through all of the animals... REPEAT the whole process
+       if you've swapped at least once
+    8. stop repeating the process when no swaps occur after going through the
+       entire list
+    9. here's an example of how it might work:
+       element 1: swap with next
+       [['zim', 'dog', 10], ['bev', 'dog', 10], ['ari', 'cat', 10]]
+       element 2: swap with next
+       [['bev', 'dog', 10], ['zim', 'dog', 10], ['ari', 'cat', 10]]
+       element 3: last element, do not compare!
+       [['bev', 'dog', 10], ['ari', 'cat', 10], ['zim', 'dog', 10]]
+
+       element 1: swap with next
+       [['bev', 'dog', 10], ['ari', 'cat', 10], ['zim', 'dog', 10]]
+       element 2: do not swap!
+       [['ari', 'cat', 10], ['bev', 'dog', 10], ['zim', 'dog', 10]]
+       element 3: last element, do not compare!
+       [['ari', 'cat', 10], ['bev', 'dog', 10], ['zim', 'dog', 10]]
+
+       element 1: do not swap!
+       [['ari', 'cat', 10], ['bev', 'dog', 10], ['zim', 'dog', 10]]
+       element 2: do not swap!
+       [['ari', 'cat', 10], ['bev', 'dog', 10], ['zim', 'dog', 10]]
+       element 3: last element, do not compare!
+       [['ari', 'cat', 10], ['bev', 'dog', 10], ['zim', 'dog', 10]]
+
+       No swaps occurred... so stop repeating process.
+
+* return value: a list of animals, alphabetically sorted by name
+
+examples:
+
+animal_list = [['sam', 'snake', 4], ['gertrude', 'goat', 99], ['ang', 'unicorn', 50]]
+sorted_animal_list = sort_by_name(animal_list)
+print(sorted_animal_list)
+# will print out [['ang', 'unicorn', 50], ['gertrude', 'goat', 99], ['sam', 'snake', 4]]
 """
